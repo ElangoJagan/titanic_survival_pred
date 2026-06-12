58fcf77c581de3a53179b7312ea1ff899867a568
ElangoJagan <elangojagan2000@gmail.com>
2026-06-12 15:08:50 +0530

    updating basics

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'titanic_survival_prediction',
+    version = "0.01"
+    author = 'ElangoJagan'
+    packages = find_packages(),
+)
